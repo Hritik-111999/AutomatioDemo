bf6f2ced044c39e9d08f399f6aeea713909910e1
Hritik-111999 <sharma.hritik1199@gmail.com>
2025-11-27 10:33:09 +0530

    trying to commit and push

---
+++ demo/src/main/java/demo/Noon_Page_Automation.java
@@ -0,0 +1,34 @@
+package demo;
+import java.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Noon_Page_Automation {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.noon.com/uae-en/");
+
+        // WebElement element1 = driver.findElement(By.xpath(""));
+        // WebElement element2 = driver.findElement(By.xpath(""));
+        // WebElement element3 = driver.findElement(By.xpath(""));
+        // WebElement element4 = driver.findElement(By.xpath(""));
+        // int count=0;
+        // while(count<11){
+        //      List<WebElement> elements = driver.findElements(By.xpath("//div[@class='flex items-center text-lg font-semibold lg:w-[145px] lg:shrink-0 lg:justify-end lg:text-base']//span[@rel='current_population']"));
+        //      for(WebElement ele : elements) {
+        //      String text = ele.getText();
+        //      Thread.sleep(1000);
+        //      System.out.println(text);
+                
+        // }
+        // count++;
+        
+        // }
+        // driver.quit();
+
+
+
+
+    }
+}
--- demo/src/main/java/demo/populationCount_Page_Automation.java
+++ demo/src/main/java/demo/populationCount_Page_Automation.java
@@ -1,11 +1,34 @@
 package demo;
-
-import org.openqa.selenium.WebDriver;
+import java.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class populationCount_Page_Automation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.worldometers.info/");
+
+        // WebElement element1 = driver.findElement(By.xpath(""));
+        // WebElement element2 = driver.findElement(By.xpath(""));
+        // WebElement element3 = driver.findElement(By.xpath(""));
+        // WebElement element4 = driver.findElement(By.xpath(""));
+        int count=0;
+        while(count<11){
+             List<WebElement> elements = driver.findElements(By.xpath("//div[@class='flex items-center text-lg font-semibold lg:w-[145px] lg:shrink-0 lg:justify-end lg:text-base']//span[@rel='current_population']"));
+             for(WebElement ele : elements) {
+             String text = ele.getText();
+             Thread.sleep(1000);
+             System.out.println(text);
+                
+        }
+        count++;
+        
+        }
+        driver.quit();
+
+
+
+
     }
 }
